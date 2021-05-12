bd8643f50fa87bafa07ec475801fc43857a398ac
David <davidstankoviic@gmail.com>
2021-05-12 21:00:34 +0200

    Add: Pages and page structure

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,6 +2,12 @@
 
 const config = {
     // firebase config
+    apiKey: "AIzaSyCsF7qfWPpcr1Zt7yi7HEmV3ILlwlbm_vQ",
+    authDomain: "instagram-clone-b9ffd.firebaseapp.com",
+    projectId: "instagram-clone-b9ffd",
+    storageBucket: "instagram-clone-b9ffd.appspot.com",
+    messagingSenderId: "166518995836",
+    appId: "1:166518995836:web:0907e2ffa0e227674182f9"
 }
 
 const firebase = window.firebase.initializeApp(config);
