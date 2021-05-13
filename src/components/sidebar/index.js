1dc9ee3a8357bc324b4432d61792f13aef288d61
David <davidstankoviic@gmail.com>
2021-05-13 19:13:44 +0200

    Add: follow suggestions sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import useUser from '../../hooks/use-user';
+import User from './user';
+import Suggestions from './suggestions';
 
 export default function Sidebar() {
-    const { user: { docId, userId, following, username, fullName } = {} } = useUser();
-    return (<p>I am the sidebar</p>)
+    const {
+        user: { fullName, username, userId }
+    } = useUser();
+
+    return (
+        <div className="p-4">
+            <User username={username} fullName={fullName} />
+            <Suggestions userId={userId} />
+        </div>
+    );
 }
